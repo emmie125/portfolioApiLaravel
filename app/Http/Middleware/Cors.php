d6c6a6dc9746f6d615eb6beb634a9d3038755280
emmie125 <mulangaemmanuella@gmail.com>
2021-11-19 17:27:41 +0100

    fix: change access control allow origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         header('Access-Control-Allow-Origin:  *');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
-        header('Access-Control-Allow-Methods:  POST, PUT');
+        header('Access-Control-Allow-Methods:  GET,PATCH,POST, PUT');
 
         return $next($request);
     }
