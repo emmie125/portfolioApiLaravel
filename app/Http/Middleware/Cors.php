a14968610cb4449c928549106bd21edb9ad3fc4f
emmie125 <mulangaemmanuella@gmail.com>
2021-11-21 22:25:48 +0100

    fix: fix a problem of cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     {
         $response = $next($request);
         $response->header('Access-Control-Allow-Methods', 'HEAD, GET, POST, PUT, DELETE');
-        $response->header('Access-Control-Allow-Headers', $request->header('Access-Control-Request-Headers'));
+        $response->header('Access-Control-Allow-Headers', 'Accept, Authorization,Content-Type');
         $response->header('Access-Control-Allow-Origin', '*');
         return $response;
     }
